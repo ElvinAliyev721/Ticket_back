a1d0205552f185b8ca290665b8491d6f8ad11575
VMammedov <vusal.memmedli25official@gmail.com>
2022-07-30 18:22:23 +0400

    Admin Panel - Blog

---
+++ Areas/AdminPanel/ViewModels/BlogsVMA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ticket.Areas.AdminPanel.ViewModels
+{
+    public class BlogsVMA
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public byte[] Image { get; set; }
+        public int Views { get; set; }
+        public bool Status { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
