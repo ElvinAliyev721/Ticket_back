45d8babc76a754e1e6cbaee4e477354038ae1b8f
Elvin <aliyeve669@gmail.com>
2022-08-03 16:53:12 +0400

    Final Version

---
+++ Areas/AdminPanel/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Ticket.DAL;
+using Ticket.Models;
+
+namespace Ticket.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    [Authorize(Roles ="Admin")]
+    public class ContactController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ContactController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult>  Index()
+        {
+            return View(await _context.Contacts.ToListAsync());
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Contact contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return View(contact);
+        }
+    }
+}
--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -22,24 +23,44 @@ namespace Ticket.Controllers
             About db_about = await _context.Abouts.FirstOrDefaultAsync();
             AboutVM aboutvm = new AboutVM();
             List<string> aboutSentences;
-            if(db_about!=null)
+            if (db_about != null)
+            {
                 if (db_about.Description != null)
                 {
-                    string[] sentences = Regex.Split(db_about.Description, @"(?<=[\.!\?])");
-                    aboutSentences = sentences.ToList();
-                    for (int i = 0; i < aboutSentences.Count; i++)
-                    {
-                        if (i < 5)
-                            aboutvm.Description_1 += aboutSentences[i];
-                        else if (i > 5 && i < 10)
-                            aboutvm.Description_2 += aboutSentences[i];
-                        else if (i > 10 && i < 15)
-                            aboutvm.Description_3 += aboutSentences[i];
-                        else
-                            aboutvm.Description_4 += aboutSentences[i];
-                    }
+                    //string[] sentences = Regex.Split(db_about.Description, @"(?<=[\.!\?])");
+                    //aboutSentences = sentences.ToList();
+                    //for (int i = 0; i < aboutSentences.Count; i++)
+                    //{
+                    //    if (i < 5)
+                    //        aboutvm.Description_1 += aboutSentences[i];
+                    //    else if (i > 5 && i < 10)
+                    //        aboutvm.Description_2 += aboutSentences[i];
+                    //    else if (i > 10 && i < 15)
+                    //        aboutvm.Description_3 += aboutSentences[i];
+                    //    else
+                    //        aboutvm.Description_4 += aboutSentences[i];
+                    //}
+
+                    IEnumerable<string> descs = CustomSplit(db_about.Description, db_about.Description.Length / 4);
+                    aboutvm.Description_1 = descs.ElementAt(0);
+                    aboutvm.Description_2 = descs.ElementAt(1);
+                    aboutvm.Description_3 = descs.ElementAt(2);
+                    aboutvm.Description_4 = descs.ElementAt(3);
                 }
+            }
+            aboutvm.Image=db_about.Image;
+            aboutvm.Title=db_about.Title;
             return View(aboutvm);
+        }
+        public IEnumerable<string> CustomSplit( string str, int n)
+        {
+            if (String.IsNullOrEmpty(str) || n < 1)
+            {
+                throw new ArgumentException();
+            }
+
+            return Enumerable.Range(0, str.Length / n)
+                            .Select(i => str.Substring(i * n, n));
         }
     }
 }
